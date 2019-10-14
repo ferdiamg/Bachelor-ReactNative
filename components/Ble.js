9d41e12b349bea9ac41d4c315efe3cbd0f2c0195
ferdiamg <ferdi.armagan@stud.hs-hannover.de>
2019-10-15 00:39:00 +0200

    Implemented Beacon found screen

--- components/Ble.js
+++ components/Ble.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import {Text, View, TouchableOpacity, StyleSheet, Alert} from 'react-native';
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+  TouchableHighlight,
+  Image,
+} from 'react-native';
 import Icon from './Icon';
 
 const styles = StyleSheet.create({
@@ -18,17 +26,53 @@ const styles = StyleSheet.create({
     fontWeight: '700',
     textAlign: 'center',
   },
+  BigText: {
+    color: '#4A4A4A',
+    fontSize: 35,
+    fontWeight: '700',
+    paddingBottom: 40,
+    paddingTop: 35,
+  },
   LowerText: {
     color: '#CFCFCF',
     fontSize: 15,
     fontWeight: '700',
     marginTop: -25,
   },
+  InfoText: {
+    color: '#CFCFCF',
+    fontSize: 15,
+    fontWeight: '700',
+    marginTop: -25,
+    textAlign: 'center',
+    paddingHorizontal: 60,
+    lineHeight: 30,
+  },
   ViewContainer: {
     display: 'flex',
     alignItems: 'center',
     marginBottom: 200,
     paddingTop: 200,
+  },
+  profileImgContainer: {
+    height: 200,
+    width: 200,
+    borderRadius: 100,
+    overflow: 'hidden',
+    marginTop: 50,
+  },
+  profileImg: {
+    marginLeft: -8,
+    height: 200,
+    width: 200,
+    borderRadius: 100,
+  },
+  Shadow: {
+    shadowColor: 'black',
+    shadowOffset: {width: 0, height: 15},
+    shadowOpacity: 0.15,
+    shadowRadius: 20,
+    elevation: 17,
   },
 });
 
@@ -39,12 +83,42 @@ class Ble extends React.Component {
 
   render() {
     return (
+      // <View>
+      //   <View style={styles.ViewContainer}>
+      //     <Icon fill="#E7E7E7" name="Wifi" width="140" height="140" />
+      //     <Text style={styles.LowerText}>Currently no Beacons found.</Text>
+      //   </View>
+      //   <View style={styles.MainContainer}>
+      //     <TouchableOpacity
+      //       style={styles.SubmitButtonStyle}
+      //       activeOpacity={0.5}
+      //       onPress={this.ButtonClickCheckFunction()}>
+      //       <Text style={styles.TextStyle}> Scan for Beacons </Text>
+      //     </TouchableOpacity>
+      //   </View>
+      // </View>
       <View>
         <View style={styles.ViewContainer}>
-          <Icon fill="#E7E7E7" name="Wifi" width="140" height="140" />
-          <Text style={styles.LowerText}>Currently no Beacons found.</Text>
-        </View>
-        <View style={styles.MainContainer}>
+          <View style={styles.Shadow}>
+            <TouchableHighlight
+              style={[
+                styles.profileImgContainer,
+                {borderColor: '#0076FF', borderWidth: 8},
+              ]}>
+              <Image
+                source={require('../images/dino.png')}
+                style={styles.profileImg}
+              />
+            </TouchableHighlight>
+          </View>
+          <Text style={styles.BigText}>T-Rex</Text>
+          <Text style={styles.InfoText}>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.
+          </Text>
           <TouchableOpacity
             style={styles.SubmitButtonStyle}
             activeOpacity={0.5}
