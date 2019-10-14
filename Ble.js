e1966113521cc9a5934c7a27ca84214bbbcb8729
ferdiamg <ferdi.armagan@stud.hs-hannover.de>
2019-10-14 21:54:11 +0200

    Added rounded button and some content for the tabs

--- Ble.js
+++ Ble.js
@@ -1,11 +1,53 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, TouchableOpacity, StyleSheet, Alert} from 'react-native';
+import Icon from './components/Icon';
+
+const styles = StyleSheet.create({
+  MainContainer: {
+    paddingTop: 200,
+  },
+  SubmitButtonStyle: {
+    paddingHorizontal: 35,
+    paddingVertical: 18,
+    backgroundColor: '#0076FF',
+    borderRadius: 40,
+    borderWidth: 0,
+    borderColor: '#fff',
+  },
+  TextStyle: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '700',
+    textAlign: 'center',
+  },
+});
 
 class Ble extends React.Component {
+  ButtonClickCheckFunction = () => () => {
+    Alert.alert('Scanning..');
+  };
+
   render() {
     return (
-      <View>
-        <Text>Hello from BLE!</Text>
+      <View style={{display: 'flex', alignItems: 'center'}}>
+        <Icon fill="#E7E7E7" name="Wifi" width="140" height="140" />
+        <Text
+          style={{
+            color: '#CFCFCF',
+            fontSize: 15,
+            fontWeight: '700',
+            marginTop: -25,
+          }}>
+          Currently no Beacons found.
+        </Text>
+        <View style={styles.MainContainer}>
+          <TouchableOpacity
+            style={styles.SubmitButtonStyle}
+            activeOpacity={0.5}
+            onPress={this.ButtonClickCheckFunction()}>
+            <Text style={styles.TextStyle}> Scan for Beacons </Text>
+          </TouchableOpacity>
+        </View>
       </View>
     );
   }
--- QR.js
+++ QR.js
@@ -4,8 +4,13 @@ import {Text, View} from 'react-native';
 class QR extends React.Component {
   render() {
     return (
-      <View>
-        <Text>Hello from QR!</Text>
+      <View style={{display: "flex", alignItems: "center", marginTop: -400}}>
+        <Text style={{color: '#4A4A4A', fontSize: 18, fontWeight: '800'}}>
+          Place the QR code inside the area
+        </Text>
+        <Text style={{color: '#CFCFCF', fontSize: 15, fontWeight: '700', paddingTop: 6}}>
+          Scanning will start automatically
+        </Text>
       </View>
     );
   }
