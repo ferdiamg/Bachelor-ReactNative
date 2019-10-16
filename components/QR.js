4ced4832f10c24890977e40fdaa56923e4cb9ba6
ferdiamg <ferdi.armagan@stud.hs-hannover.de>
2019-10-16 16:05:24 +0200

    BLE/QR scanning working

--- components/QR.js
+++ components/QR.js
@@ -1,7 +1,33 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Linking, StyleSheet, TouchableOpacity} from 'react-native';
+import QRCodeScanner from 'react-native-qrcode-scanner';
 
+const styles = StyleSheet.create({
+  centerText: {
+    flex: 1,
+    fontSize: 18,
+    padding: 32,
+    color: '#777',
+  },
+  textBold: {
+    fontWeight: '500',
+    color: '#000',
+  },
+  buttonText: {
+    fontSize: 21,
+    color: 'rgb(0,122,255)',
+  },
+  buttonTouchable: {
+    padding: 16,
+  },
+});
 class QR extends React.Component {
+  onSuccess = e => {
+    Linking.openURL(e.data).catch(err =>
+      console.error('An error occured', err),
+    );
+  };
+
   render() {
     return (
       <View style={{display: "flex", alignItems: "center", marginTop: -420}}>
@@ -11,6 +37,21 @@ class QR extends React.Component {
         <Text style={{color: '#CFCFCF', fontSize: 15, fontWeight: '700', paddingTop: 6}}>
           Scanning will start automatically
         </Text>
+        <QRCodeScanner
+          onRead={this.onSuccess}
+          topContent={
+            <Text style={styles.centerText}>
+              Go to{' '}
+              <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on
+              your computer and scan the QR code.
+            </Text>
+          }
+          bottomContent={
+            <TouchableOpacity style={styles.buttonTouchable}>
+              <Text style={styles.buttonText}>OK. Got it!</Text>
+            </TouchableOpacity>
+          }
+        />
       </View>
     );
   }
